689e5ae9cff7755107b85e4e5e8e0b5185e8348e
Ammar Shayyah <shayyahammar@gmail.com>
2026-04-25 13:34:30 +0300

    update api.php

--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,6 @@ use Illuminate\Support\Facades\Route;
 
     Route::post('/appointments/book', [UserController::class, 'book']);
 
+
+    //Route::post('/appointments/book', [UserController::class, 'book']);
 });
